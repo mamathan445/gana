65b8f66e7a426be8da3451cf407a331b0988688e
mamathan445 <66291898+mamathan445@users.noreply.github.com>
2020-06-22 19:52:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+const C4 = new Audio("sounds/C4.mp3");
+const Db4 = new Audio("sounds/Db4.mp3");
+const D4 = new Audio("sounds/D4.mp3");
+const Eb4 = new Audio("sounds/Eb4.mp3");
+const E4 = new Audio("sounds/E4.mp3");
+const F4 = new Audio("sounds/F4.mp3");
+const Gb4 = new Audio("sounds/Gb4.mp3");
+
+const playSound = audio => {
+  const clone = audio.cloneNode();
+  clone.play();
+  setTimeout(() => (clone.volume = 0.8), 400);
+  setTimeout(() => (clone.volume = 0.6), 800);
+  setTimeout(() => (clone.volume = 0.4), 1200);
+  setTimeout(() => (clone.volume = 0.2), 1600);
+  setTimeout(() => (clone.volume = 0), 2000);
+}
+
+// C4
+const C4Key = document.querySelector(".C4-key");
+const playC4 = () => {
+  playSound(C4);
+  C4Key.classList.add("active");
+  setTimeout(() => C4Key.classList.remove("active"), 200);
+};
+C4Key.addEventListener("click", playC4);
+
+// Db4
+const Db4Key = document.querySelector(".Db4-key");
+const playDb4 = () => {
+  playSound(Db4);
+  Db4Key.classList.add("active");
+  setTimeout(() => Db4Key.classList.remove("active"), 200);
+};
+Db4Key.addEventListener("click", playDb4);
+
+// D4
+const D4Key = document.querySelector(".D4-key");
+const playD4 = () => {
+  playSound(D4);
+  D4Key.classList.add("active");
+  setTimeout(() => D4Key.classList.remove("active"), 200);
+};
+D4Key.addEventListener("click", playD4);
+
+// Eb4
+const Eb4Key = document.querySelector(".Eb4-key");
+const playEb4 = () => {
+  playSound(Eb4);
+  Eb4Key.classList.add("active");
+  setTimeout(() => Eb4Key.classList.remove("active"), 200);
+};
+Eb4Key.addEventListener("click", playEb4);
+
+// E4
+const E4Key = document.querySelector(".E4-key");
+const playE4 = () => {
+  playSound(E4);
+  E4Key.classList.add("active");
+  setTimeout(() => E4Key.classList.remove("active"), 200);
+};
+E4Key.addEventListener("click", playE4);
+
+// F4
+const F4Key = document.querySelector(".F4-key");
+const playF4 = () => {
+  playSound(F4);
+  F4Key.classList.add("active");
+  setTimeout(() => F4Key.classList.remove("active"), 200);
+};
+F4Key.addEventListener("click", playF4);
+
+// Gb4
+const Gb4Key = document.querySelector(".Gb4-key");
+const playGb4 = () => {
+  playSound(Gb4);
+  Gb4Key.classList.add("active");
+  setTimeout(() => Gb4Key.classList.remove("active"), 200);
+};
+Gb4Key.addEventListener("click", playGb4);
+
+
+window.addEventListener("keydown", ({ keyCode }) => {
+  // Press L
+  if (keyCode === 76) return playC4();
+
+  // Press A
+  if (keyCode === 65) return playDb4();
+
+  // Press K
+  if (keyCode === 75) return playD4();
+
+  // Press S
+  if (keyCode === 83) return playEb4();
+
+  // Press H
+  if (keyCode === 72) return playE4();
+
+  // Press M
+  if (keyCode === 77) return playF4();
+
+  // Press I
+  if (keyCode === 73) return playGb4();
+
+} )
